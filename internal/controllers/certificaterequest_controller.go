5d1cddca04ff369e4a1bde9b7ec0aaff6f764d71
Antonin <antonin.guyot@icloud.com>
2022-07-06 14:34:32 +0200

    Set labels, teams, and owners on certificates through annotations

--- internal/controllers/certificaterequest_controller.go
+++ internal/controllers/certificaterequest_controller.go
@@ -20,6 +20,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/evertrust/horizon-go/http"
+	"github.com/evertrust/horizon-go/requests"
 	horizonapi "github.com/evertrust/horizon-issuer/api/v1alpha1"
 	horizonissuer "github.com/evertrust/horizon-issuer/internal/issuer/horizon"
 	issuerutil "github.com/evertrust/horizon-issuer/internal/issuer/util"
@@ -27,6 +29,7 @@ import (
 	cmapi "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1"
 	cmmeta "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -35,16 +38,13 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var (
 	errIssuerRef      = errors.New("error interpreting issuerRef")
 	errGetIssuer      = errors.New("error getting issuer")
 	errIssuerNotReady = errors.New("issuer is not ready")
-	errSignerBuilder  = errors.New("failed to build the signer")
-	errSignerSign     = errors.New("failed to sign")
-	errInvalidBaseUrl = errors.New("invalid base url")
-	errUnknownHorizon = errors.New("horizon returned an error")
 )
 
 const FinalizerName = horizonissuer.IssuerNamespace + "/finalizer"
@@ -56,7 +56,6 @@ type CertificateRequestReconciler struct {
 	ClusterResourceNamespace string
 	Clock                    clock.Clock
 	Issuer                   horizonissuer.HorizonIssuer
-	RevokeCertificates       bool
 }
 
 func (r *CertificateRequestReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
@@ -90,39 +89,22 @@ func (r *CertificateRequestReconciler) Reconcile(ctx context.Context, req ctrl.R
 		)
 	}
 
-	// Ignore but log an error if the issuerRef.Kind is unrecognised
-	issuerGVK := horizonapi.GroupVersion.WithKind(certificateRequest.Spec.IssuerRef.Kind)
-	issuerRO, err := r.Scheme.New(issuerGVK)
+	issuer, err := r.issuerFromRequest(ctx, &certificateRequest)
 	if err != nil {
-		err = fmt.Errorf("%w: %v", errIssuerRef, err)
-		log.Error(err, "Unrecognised kind. Ignoring.")
-		setReadyCondition(cmmeta.ConditionFalse, cmapi.CertificateRequestReasonFailed, err.Error())
-		return ctrl.Result{}, nil
-	}
-	issuer := issuerRO.(client.Object)
-	// Create a Namespaced name for Issuer and a non-Namespaced name for ClusterIssuer
-	issuerName := types.NamespacedName{
-		Name: certificateRequest.Spec.IssuerRef.Name,
-	}
-	var secretNamespace string
-	switch t := issuer.(type) {
-	case *horizonapi.Issuer:
-		issuerName.Namespace = certificateRequest.Namespace
-		secretNamespace = certificateRequest.Namespace
-		log = log.WithValues("issuer", issuerName)
-	case *horizonapi.ClusterIssuer:
-		secretNamespace = r.ClusterResourceNamespace
-		log = log.WithValues("clusterissuer", issuerName)
-	default:
-		err := fmt.Errorf("unexpected issuer type: %v", t)
-		log.Error(err, "The issuerRef referred to a registered Kind which is not yet handled. Ignoring.")
-		setReadyCondition(cmmeta.ConditionFalse, cmapi.CertificateRequestReasonFailed, err.Error())
-		return ctrl.Result{}, nil
+		log.Error(err, "Cannot find Issuer")
+		return ctrl.Result{}, fmt.Errorf("%w", err)
 	}
 
-	// Get the Issuer or ClusterIssuer
-	if err := r.Get(ctx, issuerName, issuer); err != nil {
-		return ctrl.Result{}, fmt.Errorf("%w: %v", errGetIssuer, err)
+	var secretNamespace string
+	switch issuer.(type) {
+	case *horizonapi.Issuer:
+		secretNamespace = certificateRequest.Namespace
+		log = log.WithValues("issuer", issuer.GetName())
+	case *horizonapi.ClusterIssuer:
+		secretNamespace = r.ClusterResourceNamespace
+		log = log.WithValues("clusterissuer", issuer.GetName())
+	default:
+		return ctrl.Result{}, nil
 	}
 
 	issuerSpec, issuerStatus, err := issuerutil.GetSpecAndStatus(issuer)
@@ -238,7 +220,11 @@ func (r *CertificateRequestReconciler) Reconcile(ctx context.Context, req ctrl.R
 		if _, ok := certificateRequest.Annotations[horizonissuer.RequestIdAnnotation]; ok {
 			return r.Issuer.UpdateRequest(ctx, &certificateRequest)
 		} else {
-			return r.Issuer.SubmitRequest(ctx, r.Client, issuerSpec.Profile, &certificateRequest)
+			labels, owner, team, err := r.certificateMetadata(ctx, &certificateRequest)
+			if err != nil {
+				setReadyCondition(cmmeta.ConditionFalse, cmapi.CertificateRequestReasonPending, err.Error())
+			}
+			return r.Issuer.SubmitRequest(ctx, r.Client, *issuerSpec, labels, owner, team, &certificateRequest)
 		}
 	}
 
@@ -250,8 +236,12 @@ func (r *CertificateRequestReconciler) handleDeletion(ctx context.Context, certi
 		// our finalizer is present, so lets handle any external dependency
 		if err := r.Issuer.RevokeCertificate(ctx, certificateRequest); err != nil {
 			// if fail to delete the external dependency here, return with error
-			// so that it can be retried
-			return err
+			// so that it can be retried, except if the error is from Horizon
+			if _, isHorizonError := err.(*http.HorizonErrorResponse); !isHorizonError {
+				return err
+			} else {
+				log.FromContext(ctx).Info(fmt.Sprintf("Horizon returned an error when revoking the certificate : %s. Marking the certificate as revoked to avoid a loop.", err.Error()))
+			}
 		}
 
 		// remove our finalizer from the list and update it.
@@ -262,6 +252,136 @@ func (r *CertificateRequestReconciler) handleDeletion(ctx context.Context, certi
 	}
 
 	return nil
+}
+
+func (r *CertificateRequestReconciler) certificateMetadata(ctx context.Context, certificateRequest *cmapi.CertificateRequest) ([]requests.LabelElement, *string, *string, error) {
+	// Récupérer le certificat
+	var owner *string
+	var team *string
+	var labels []requests.LabelElement
+
+	certificate, err := r.certificateFromRequest(ctx, certificateRequest)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	issuer, err := r.issuerFromRequest(ctx, certificateRequest)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	ingress, err := r.ingressFromCertificate(ctx, certificate)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	if ingress != nil {
+		ownerString := ingress.Annotations[horizonissuer.OwnerAnnotation]
+		if ownerString != "" {
+			owner = &ownerString
+		}
+		teamString := ingress.Annotations[horizonissuer.TeamAnnotation]
+		if teamString != "" {
+			owner = &teamString
+		}
+	}
+
+	if certificate != nil {
+		ownerString := certificate.Annotations[horizonissuer.OwnerAnnotation]
+		if ownerString != "" {
+			owner = &ownerString
+		}
+		teamString := certificate.Annotations[horizonissuer.TeamAnnotation]
+		if teamString != "" {
+			owner = &teamString
+		}
+	}
+
+	issuerSpec, _, err := issuerutil.GetSpecAndStatus(issuer)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	if issuerSpec.Owner != nil {
+		owner = issuerSpec.Owner
+	}
+
+	if issuerSpec.Team != nil {
+		team = issuerSpec.Team
+	}
+
+	if len(issuerSpec.Labels) > 0 {
+		for k, v := range issuerSpec.Labels {
+			labels = append(labels, requests.LabelElement{
+				Label: k,
+				Value: v,
+			})
+		}
+	}
+
+	return labels, owner, team, nil
+}
+
+// issuerFromRequest returns the Issuer of a given CertificateRequest.
+func (r *CertificateRequestReconciler) issuerFromRequest(ctx context.Context, certificateRequest *cmapi.CertificateRequest) (client.Object, error) {
+	issuerGVK := horizonapi.GroupVersion.WithKind(certificateRequest.Spec.IssuerRef.Kind)
+	issuerRO, err := r.Scheme.New(issuerGVK)
+	if err != nil {
+		err = fmt.Errorf("%w: %v", errIssuerRef, err)
+		return nil, err
+	}
+	issuer := issuerRO.(client.Object)
+	// Create a Namespaced name for Issuer and a non-Namespaced name for ClusterIssuer
+	issuerName := types.NamespacedName{
+		Name: certificateRequest.Spec.IssuerRef.Name,
+	}
+	switch t := issuer.(type) {
+	case *horizonapi.Issuer:
+		issuerName.Namespace = certificateRequest.Namespace
+	case *horizonapi.ClusterIssuer:
+	default:
+		err := fmt.Errorf("unexpected issuer type: %v", t)
+		return nil, err
+	}
+
+	// Get the Issuer or ClusterIssuer
+	if err := r.Get(ctx, issuerName, issuer); err != nil {
+		return nil, fmt.Errorf("%w: %v", errGetIssuer, err)
+	}
+
+	return issuer, nil
+
+}
+
+// certificateFromRequest returns the Certificate object associated with that CertificateRequest
+func (r *CertificateRequestReconciler) certificateFromRequest(ctx context.Context, certificateRequest *cmapi.CertificateRequest) (*cmapi.Certificate, error) {
+	certificateName := types.NamespacedName{
+		Namespace: certificateRequest.Namespace,
+		Name:      certificateRequest.Annotations["cert-manager.io/certificate-name"],
+	}
+
+	var certificate cmapi.Certificate
+	err := r.Get(ctx, certificateName, &certificate)
+
+	return &certificate, err
+}
+
+func (r *CertificateRequestReconciler) ingressFromCertificate(ctx context.Context, certificate *cmapi.Certificate) (*v1.Ingress, error) {
+	var ingressName *types.NamespacedName
+	for _, ref := range certificate.OwnerReferences {
+		if ref.APIVersion == "networking.k8s.io/v1" && ref.Kind == "Ingress" {
+			ingressName = &types.NamespacedName{
+				Namespace: certificate.Namespace,
+				Name:      ref.Name,
+			}
+		}
+	}
+
+	if ingressName == nil {
+		return nil, nil
+	}
+
+	var ingress v1.Ingress
+	err := r.Get(ctx, *ingressName, &ingress)
+	return &ingress, err
 }
 
 func (r *CertificateRequestReconciler) SetupWithManager(mgr ctrl.Manager) error {
