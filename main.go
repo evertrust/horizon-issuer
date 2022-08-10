1d344bc8245b1e5c30685473d2f92da1cd1255a1
Antonin <antonin.guyot@icloud.com>
2022-08-10 14:20:47 +0200

    Health check for the Issuer resource

--- main.go
+++ main.go
@@ -121,6 +121,7 @@ func main() {
 		Client:                   mgr.GetClient(),
 		Scheme:                   mgr.GetScheme(),
 		ClusterResourceNamespace: clusterResourceNamespace,
+		HealthCheckerBuilder:     horizon.HorizonHealthCheckerFromIssuer,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Issuer")
 		os.Exit(1)
